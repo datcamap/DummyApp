7878db4a4ed22278a3fcc37aaaa73cf98829e60d
Datcamap <48880635+datcamap@users.noreply.github.com>
2023-01-08 18:00:48 +0700

    home UI commit

--- app/src/main/java/com/example/dummyapp/MainActivity.java
+++ app/src/main/java/com/example/dummyapp/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.dummyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    boolean onHeat = true;
+    boolean onChill = false;
+    boolean onAuto = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,29 +27,109 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         setTitle("Home");
 
-        ImageButton settingButt= (ImageButton) findViewById(R.id.buttonSetting);
+        ImageButton settingButt= findViewById(R.id.buttonSetting);
         settingButt.setOnClickListener(this);
-        Button detailButt= (Button) findViewById(R.id.buttonDetail);
+        Button detailButt= findViewById(R.id.buttonDetail);
         detailButt.setOnClickListener(this);
-        Button heaterButt= (Button) findViewById(R.id.buttonHeater);
+        Button heaterButt= findViewById(R.id.buttonHeater);
         heaterButt.setOnClickListener(this);
+        Button chillerButt= findViewById(R.id.buttonChiller);
+        chillerButt.setOnClickListener(this);
+        Button modeButt= findViewById(R.id.buttonMode);
+        modeButt.setOnClickListener(this);
+        SeekBar yourSeekBar= findViewById(R.id.seekBar2);
+        yourSeekBar.setOnSeekBarChangeListener(new seekHandler());
+
+        heatButtHandler();
+        chillButtHandler();
     }
 
     @Override
     public void onClick(View v) {
-        ImageButton settingButt= (ImageButton) findViewById(R.id.buttonSetting);
+        ImageButton settingButt= findViewById(R.id.buttonSetting);
         switch (v.getId()) {
             case R.id.buttonSetting:
                 settingButt.setEnabled(false);
                 break;
             case R.id.buttonDetail:
-                Toast.makeText(this,"clicked",Toast.LENGTH_LONG).show();
+                Toast.makeText(this,"Clicked",Toast.LENGTH_LONG).show();
+                Intent i = new Intent(this,ChartView.class);
+                startActivity(i);
                 break;
             case R.id.buttonHeater:
-                Toast.makeText(this,"heated",Toast.LENGTH_LONG).show();
+                heatButtHandler();
+                break;
+            case R.id.buttonChiller:
+                chillButtHandler();
+                break;
+            case R.id.buttonMode:
+                Toast.makeText(this,"Mode Changed",Toast.LENGTH_SHORT).show();
+                onAuto = !onAuto;
+                if (onAuto) {
+                    ((TextView) findViewById(R.id.modeState)).setText(R.string.autoMode);
+                }
+                else {
+                    ((TextView) findViewById(R.id.modeState)).setText(R.string.manuMode);
+                }
                 break;
             default:
                 break;
         }
+    }
+
+    public void heatButtHandler() {
+        if(onAuto) {
+            Toast.makeText(this,"Auto is ON",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            onHeat = !onHeat;
+        }
+        ConstraintLayout heatButt = findViewById(R.id.heatButtHolder);
+        TextView txt = findViewById(R.id.heatOnIndicator);
+        if(onHeat) {
+            txt.setText(R.string.on);
+            heatButt.setBackgroundTintList(AppCompatResources.getColorStateList(this,R.color.butt_oncolor));
+            txt.setTypeface(null, Typeface.BOLD);        }
+        else {
+            txt.setText(R.string.off);
+            heatButt.setBackgroundTintList(AppCompatResources.getColorStateList(this,R.color.butt_offcolor));
+            txt.setTypeface(null, Typeface.NORMAL);
+        }
+    }
+
+    public void chillButtHandler() {
+        if(onAuto) {
+            Toast.makeText(this,"Auto is ON",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            onChill = !onChill;
+        }
+        ConstraintLayout heatButt = findViewById(R.id.chillButtHolder);
+        TextView txt = findViewById(R.id.chillOnIndicator);
+        if(onChill) {
+            txt.setText(R.string.on);
+            heatButt.setBackgroundTintList(AppCompatResources.getColorStateList(this,R.color.butt_oncolor));
+            txt.setTypeface(null, Typeface.BOLD);        }
+        else {
+            txt.setText(R.string.off);
+            heatButt.setBackgroundTintList(AppCompatResources.getColorStateList(this,R.color.butt_offcolor));
+            txt.setTypeface(null, Typeface.NORMAL);
+        }
+    }
+
+    private class seekHandler implements SeekBar.OnSeekBarChangeListener {
+
+        public void onProgressChanged(SeekBar seekBar, int progress,
+                                      boolean fromUser) {
+            // Log the progress
+            Log.d("DEBUG", "Progress is: "+progress);
+            //set textView's text
+            ((TextView)findViewById(R.id.temperValue)).setText(""+progress);
+        }
+
+        public void onStartTrackingTouch(SeekBar seekBar) {}
+
+        public void onStopTrackingTouch(SeekBar seekBar) {}
+
     }
 }
